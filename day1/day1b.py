6f7b9579bb42683abd72f734c5de3fb24145d148
Gina Alfandary Magana <ginaleealfy@gmail.com>
2021-12-02 18:36:59 -0800

    Optimized solution

--- day1/day1b.py
+++ day1/day1b.py
@@ -1,25 +1,23 @@
-
-currentValue = -1
 numberOfIncreases = 0
 fileAsList = []
+start = 0
+end = 3
 
 # Open the input file
 with open("day1\input1a.txt") as puzzleFile:
     # For every line in the file
     fileAsList = puzzleFile.readlines()
 
-# Iterate through the list size minus 2
-for index in range(len(fileAsList)- 2):
-    # Get the next three values
-    currentWindow0 = int(fileAsList[index])
-    currentWindow1 = int(fileAsList[index+1])
-    currentWindow2 = int(fileAsList[index+2])
-    currentSum = currentWindow0 + currentWindow1 + currentWindow2
-    # if there is an increase in depth and its not the first value
-    if currentSum > currentValue and currentValue != -1:
+# as long as end is a valid index
+while end < len(fileAsList):
+    # Get the integer values
+    startValue = int(fileAsList[start])
+    endValue = int(fileAsList[end])
+    # Compare the new value against the old
+    if startValue < endValue:
         numberOfIncreases += 1
-    # Eitherway we want to update our current depth
-    currentValue = currentSum
+    start += 1
+    end += 1
 
 # Print out final number of increases 
 print(numberOfIncreases)
